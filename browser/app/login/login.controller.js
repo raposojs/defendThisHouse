bde07bed32fcde7ec26bba5d2aeff9c90dcab2ec
Danny Kearney <danielfelixkearney@gmail.com>
2016-09-01 15:36:21 -0400

    more cleaner

--- browser/app/login/login.controller.js
+++ browser/app/login/login.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
-app.controller('LoginCtrl', function ($scope, Auth, $state) {
+app.controller('LoginCtrl', function ($scope, Auth, $state, $sanitize) {
   $scope.loginUser = function (credentials) {
+  	credentials = $sanitize(credentials)
     Auth.login(credentials)
     .then(function (loggedinUser) {
       $state.go('user', {id: loggedinUser.id});
--- browser/app/signup/signup.controller.js
+++ browser/app/signup/signup.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
-app.controller('SignupCtrl', function ($scope, Auth, $state) {
+app.controller('SignupCtrl', function ($scope, Auth, $state, $sanitize) {
   $scope.signupUser = function (credentials) {
+  	credentials = $sanitize(credentials)
     Auth.signup(credentials)
     .then(function (loggedinUser) {
       $state.go('user', {id: loggedinUser.id});
